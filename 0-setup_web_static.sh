8a7417cb704e024a5af488264d76d1a77188c739
cyuzuzogermain <c.germain@alustudent.com>
2026-02-10 12:04:31 +0200

    #

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -29,9 +29,9 @@ sudo chown -R ubuntu:ubuntu /data/
 # We use 'printf' to handle the multiline block and 'sed' to inject it
 # The check prevents adding the block multiple times if the script is rerun
 NGINX_CONF="/etc/nginx/sites-available/default"
-STATIC_BLOCK="location /hbnb_static {\n\talias /data/web_static/current/;\n}"
+STATIC_BLOCK="location 127.0.0.1/hbnb_static {\n\talias /data/web_static/current/;\n}"
 
-if ! grep -q "location /hbnb_static" "$NGINX_CONF"; then
+if ! grep -q "location 127.0.0.1/hbnb_static" "$NGINX_CONF"; then
     sudo sed -i "/server_name _;/a \\\n\t$STATIC_BLOCK" "$NGINX_CONF"
 fi
 
