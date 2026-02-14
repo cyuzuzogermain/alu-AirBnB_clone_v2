2ed9c88fd4711221f3b6e732da9677897900230f
cyuzuzogermain <c.germain@alustudent.com>
2026-02-14 20:32:21 +0200

    modified do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,65 +1,30 @@
 #!/usr/bin/python3
 """
-Fabric script that creates and distributes an archive to web servers
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
-from fabric.api import env, local, put, run
-from datetime import datetime
-import os
 
-# Define your server IPs
+from fabric.api import put, run, env
+from os.path import exists
 env.hosts = ['34.205.24.28', '34.239.101.72']
-env.user = 'ubuntu'
 
-def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder."""
-    try:
-        now = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_path = "versions/web_static_{}.tgz".format(now)
-
-        if not os.path.exists("versions"):
-            local("mkdir -p versions")
-
-        local("tar -cvzf {} web_static".format(archive_path))
-        return archive_path
-    except Exception:
-        return None
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers."""
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        # Get filename info
-        file_name = os.path.basename(archive_path)
-        folder_name = file_name.split(".")[0]
-        remote_tmp = "/tmp/{}".format(file_name)
-        release_path = "/data/web_static/releases/{}/".format(folder_name)
-
-        # 1. Upload
-        put(archive_path, remote_tmp)
-
-        # 2. Uncompress and setup directory
-        run("mkdir -p {}".format(release_path))
-        run("tar -xzf {} -C {}".format(remote_tmp, release_path))
-        run("rm {}".format(remote_tmp))
-
-        # 3. Flatten the structure (Move files out of web_static subfolder)
-        # This ensures /hbnb_static/0-index.html works correctly
-        run("mv {}web_static/* {}".format(release_path, release_path))
-        run("rm -rf {}web_static".format(release_path))
-
-        # 4. Update symbolic link
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(release_path))
-
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception:
+    except:
         return False
-
-def deploy():
-    """Runs the full pack and deploy process."""
-    path = do_pack()
-    if path is None:
-        return False
-    return do_deploy(path)
